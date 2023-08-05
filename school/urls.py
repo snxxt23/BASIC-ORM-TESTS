cc32a9d165c228a62f68f46837fc19bdaf4fede7
Sandeep Nadh <snxxt23@gmail.com>
2023-08-05 15:42:59 +0530

    unionquery

--- school/urls.py
+++ school/urls.py
@@ -6,4 +6,5 @@ app_name = 'school'
 urlpatterns = [
     path('or',views.student_list,name='student_list'),
     path('and',views.andquery,name="andquery"),
+    path('union',views.unionquery,name='unionquery'),
 ]
--- school/views.py
+++ school/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import Student
+from .models import Student,Teacher
 from django.db import connection
+
 
 
 # Create your views here.
@@ -31,3 +32,9 @@ def andquery(request):
     print(connection.queries)
     return render(request,'output.html',{'posts':posts})
 
+def unionquery(request):
+    posts=Student.objects.all().values_list("firstname").union(Teacher.objects.all().values_list("firstname"))
+    # .values_list is used to retreive a list of tuples containing specific fields from a models queryset
+    print(posts)
+    # print(connection.queries)
+    return render (request,'output.html',{'posts':posts})
